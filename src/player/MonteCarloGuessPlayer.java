f98358686aeb7a0bfe35da8ab0dc5dfa16559daf
lionel-penington <lioneljpenington@gmail.com>
2017-05-25 21:52:03 +1000

    Begun working w/ killer odds, compiles clean

--- src/player/MonteCarloGuessPlayer.java
+++ src/player/MonteCarloGuessPlayer.java
@@ -3,6 +3,7 @@ package player;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.PriorityQueue;
 import world.World;
 
 /**
@@ -12,13 +13,10 @@ import world.World;
  * @author Youhan, Jeffrey
  */
 public class MonteCarloGuessPlayer extends RandomGuessPlayer implements Player{
-
-    MonteCoord lockedTarget = null;
+    public MonteCoord lockedTarget = null;
+    public boolean lockChanged;//TODO: consider moving to a method
     public ArrayList<MonteCoord> strikes;
-    public ArrayList<World.Coordinate> shotsFired = new ArrayList<>();
     public MonteCoord[][] seekGrid;
-    public ArrayList<MonteCoord> xTargetGrid;
-    public ArrayList<MonteCoord> yTargetGrid;
     public int[] targetsLeft;
     public int longestTarget;
     @Override
@@ -38,6 +36,7 @@ public class MonteCarloGuessPlayer extends RandomGuessPlayer implements Player{
         targetsLeft[4] = 5;
         longestTarget = 5;
         strikes = new ArrayList<MonteCoord>();
+        lockChanged = false;
     } // end of initialisePlayer()
 
     public void populateTargetGrid()
@@ -98,45 +97,19 @@ public class MonteCarloGuessPlayer extends RandomGuessPlayer implements Player{
     @Override
     public Guess makeGuess() {
         // To be implemented.
+        MonteCoord chosenTarget;
         if(lockedTarget == null || lockedTarget.state != 'h')
         {//Seeking target
-            MonteCoord chosenTarget = fetchBestSeek();
-            Guess target = new Guess();
-            target.column = chosenTarget.col;
-            target.row = chosenTarget.row;
-            return target;
+            chosenTarget = fetchBestSeek();
         }
         else//TODO: Cleanup after killing a target?
         {//Eliminating target
-            if(strikes.size() == 1)
-            {
-                //clean target seeking
-                int width  = targetWidth(strikes.get(0).col,strikes.get(0).row);
-                int height = targetHeight(strikes.get(0).col,strikes.get(0).row);
-                int runningTotalOdds = 0;
-                int i;
-                /*for(i = 0; i < targetsLeft.length; i++)
-                {
-                    if(targetsLeft[i] != 0 && width - targetsLeft[i] > 0)
-                    {
-                        runningTotalOdds += Math.min(targetsLeft[i], width - targetsLeft[i] + 1 - strikes.size());
-                    }
-                    if(targetsLeft[i] != 0 && height - targetsLeft[i] > 0)
-                    {
-                        runningTotalOdds += Math.min(targetsLeft[i], height - targetsLeft[i] + 1 - strikes.size());
-                    }
-                    i++;
-                }
-                */
-            }
-            else
-            {
-                //multi-point seeking
-                
-            }
+            chosenTarget = fetchBestKill();
         }
-        // dummy return
-        return null;
+        Guess target = new Guess();
+        target.column = chosenTarget.col;
+        target.row = chosenTarget.row;
+        return target;
     } // end of makeGuess()
 
     public MonteCoord fetchBestSeek()
@@ -166,6 +139,33 @@ public class MonteCarloGuessPlayer extends RandomGuessPlayer implements Player{
         return bestSquares.get(randIndex);
     }
     
+    public MonteCoord fetchBestKill()
+    {
+        int x, y, best = 0;
+        ArrayList<MonteCoord> bestSquares = new ArrayList<MonteCoord>();
+        for(x = 0; x < numCol; x++)
+        {
+            for(y = 0; y < numRow; y ++)
+            {
+                if(seekGrid[x][y].killerOdds > best)
+                {
+                    bestSquares.clear();
+                    best = seekGrid[x][y].killerOdds;
+                    bestSquares.add(seekGrid[x][y]);
+                }
+                else
+                {
+                    if(seekGrid[x][y].killerOdds == best)
+                    {
+                        bestSquares.add(seekGrid[x][y]);
+                    }
+                }
+            }
+        }
+        int randIndex = this.random.nextInt(bestSquares.size());
+        return bestSquares.get(randIndex);
+    }
+    
     @Override
     public void update(Guess guess, Answer answer) {
         // To be implemented.
@@ -176,6 +176,7 @@ public class MonteCarloGuessPlayer extends RandomGuessPlayer implements Player{
                 seekGrid[guess.column][guess.row].state = 'h';
                 seekGrid[guess.column][guess.row].odds = 0;
                 lockedTarget = seekGrid[guess.column][guess.row];
+                lockChanged = true;
                 strikes.add(seekGrid[guess.column][guess.row]);
             }
             else//Hit, sunk
@@ -183,7 +184,7 @@ public class MonteCarloGuessPlayer extends RandomGuessPlayer implements Player{
                 seekGrid[guess.column][guess.row].state = 's';
                 seekGrid[guess.column][guess.row].odds = 0;
                 clearStrikes();//TODO: IMPLEMENT THIS
-                lockedTarget = null;
+                //lockedTarget = null;
                 int sunkIndex;
                 switch(answer.shipSunk.name())
                 {
@@ -207,7 +208,6 @@ public class MonteCarloGuessPlayer extends RandomGuessPlayer implements Player{
                         System.out.println("Error: Bad sunk name");
                 }
                 //TODO Removing strikes here
-                //strikes -= targetsLeft[sunkIndex];
                 targetsLeft[sunkIndex] = 0;
                 int i = targetsLeft.length - 1;
                 while(targetsLeft[i] == 0)
@@ -216,6 +216,7 @@ public class MonteCarloGuessPlayer extends RandomGuessPlayer implements Player{
                 }
                 longestTarget = targetsLeft[i];
                 oddsUpdate(targetsLeft);
+                //Lock changed?
             }
         }
         else//Missed
@@ -223,6 +224,11 @@ public class MonteCarloGuessPlayer extends RandomGuessPlayer implements Player{
             seekGrid[guess.column][guess.row].state = 'm';
             seekGrid[guess.column][guess.row].odds = 0;
             oddsUpdate(guess);
+            lockChanged = false;
+        }
+        if(lockChanged)
+        {
+            updateKillerOdds(seekGrid[guess.column][guess.row]);
         }
     } // end of update()
     
@@ -275,6 +281,103 @@ public class MonteCarloGuessPlayer extends RandomGuessPlayer implements Player{
         }
         seekGrid[x][y].odds = runningTotalOdds;
     }
+    
+    public void updateKillerOdds(MonteCoord givenLock)
+    {
+        int runningTotalOdds = 0;
+        int i, ships, spacesAbove,spacesBelow,spacesRight,spacesLeft, maxVal;
+        boolean stillClear;
+        for(ships = 0; ships < targetsLeft.length; ships++)
+        {
+            i = 0;
+            spacesAbove = 0;
+            spacesBelow = 0;
+            spacesRight = 0;
+            spacesLeft  = 0;
+            maxVal = 0;
+            stillClear = true;
+            //Working through each direction, finding how far it can go in that direction
+            while( i < targetsLeft[ships] && stillClear && givenLock.col - i >= 0)
+            {
+                if(seekGrid[givenLock.col - i][givenLock.row].state == 'h' || seekGrid[givenLock.col - i][givenLock.row].state == 'n')
+                {
+                    i++;
+                    spacesLeft++;
+                }
+                else
+                {
+                    stillClear = false;
+                }
+            }
+            i = 0;
+            stillClear = true;
+            while( i < targetsLeft[ships] && stillClear && givenLock.col + i <= this.numCol)
+            {
+                if(seekGrid[givenLock.col + i][givenLock.row].state == 'h' || seekGrid[givenLock.col + i][givenLock.row].state == 'n')
+                {
+                    i++;
+                    spacesRight++;
+                }
+                else
+                {
+                    stillClear = false;
+                }
+            }
+            i = 0;
+            stillClear = true;
+            while( i < targetsLeft[ships] && stillClear && givenLock.row + i <= this.numRow)
+            {
+                if(seekGrid[givenLock.col][givenLock.row + i].state == 'h' || seekGrid[givenLock.col][givenLock.row + i].state == 'n')
+                {
+                    i++;
+                    spacesAbove++;
+                }
+                else
+                {
+                    stillClear = false;
+                }
+            }
+            i = 0;
+            stillClear = true;
+            while( i < targetsLeft[ships] && stillClear && givenLock.col - i >= 0)
+            {
+                if(seekGrid[givenLock.col][givenLock.row - i].state == 'h' || seekGrid[givenLock.col][givenLock.row - i].state == 'n')
+                {
+                    i++;
+                    spacesBelow++;
+                }
+                else
+                {
+                    stillClear = false;
+                }
+            }
+            //Iterating through each axis, adding up the expected
+            if(spacesAbove + spacesBelow > targetsLeft[ships])
+            {
+                maxVal = Math.min(targetsLeft[ships] - 1, spacesAbove + spacesBelow + 1 - targetsLeft[ships]);
+                for(i = spacesAbove; i > 0; i--)
+                {
+                    seekGrid[givenLock.col][givenLock.row + i].killerOdds += Math.min(maxVal, i);
+                }
+                for(i = spacesBelow; i > 0; i--)
+                {
+                    seekGrid[givenLock.col][givenLock.row - i].killerOdds += Math.min(maxVal, i);
+                }
+            }
+            if(spacesLeft + spacesRight > targetsLeft[ships])
+            {
+                maxVal = Math.min(targetsLeft[ships] - 1, spacesLeft + spacesRight + 1 - targetsLeft[ships]);
+                for(i = spacesLeft; i > 0; i--)
+                {
+                    seekGrid[givenLock.col - i][givenLock.row].killerOdds += Math.min(maxVal, i);
+                }
+                for(i = spacesRight; i > 0; i--)
+                {
+                    seekGrid[givenLock.col + i][givenLock.row].killerOdds += Math.min(maxVal, i);
+                }
+            }
+        }
+    }//End of updateKillerOdds
     
     //Method for finding how many spaces are free to the left and right of a target
     public int targetWidth(int col, int row)
--- src/player/MonteCoord.java
+++ src/player/MonteCoord.java
@@ -1,14 +1,16 @@
 package player;
-public class MonteCoord implements Comparable{
+public class MonteCoord{
     int row;
     int col;
     int odds;
+    int killerOdds;//Used when finishing off a ship
     char state;//n for nothing, h for active hit, s for sunk, m for miss
     public MonteCoord(int x, int y)
     {
         this.col = x;
         this.row = y;
         this.odds = 0;
+        this.killerOdds = 0;
         this.state = 'n';
     }
     public MonteCoord(int x, int y, int odds)
@@ -16,10 +18,7 @@ public class MonteCoord implements Comparable{
         this.col = x;
         this.row = y;
         this.odds = odds;
+        this.killerOdds = 0;
         this.state = 'n';
-    }
-    public int compareTo(MonteCoord other)
-    {
-        return this.odds - other.odds;
     }
 }
